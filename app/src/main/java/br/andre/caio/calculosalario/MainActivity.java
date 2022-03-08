d2fced3a25cb866bbfbf608abd6dc2aab7feb5e1
Andre Rodrigues <andrerodrisantos15@gmail.com>
2022-03-08 10:24:24 -0300

    Modifições no calculo

--- app/src/main/java/br/andre/caio/calculosalario/MainActivity.java
+++ app/src/main/java/br/andre/caio/calculosalario/MainActivity.java
@@ -13,14 +13,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.time.Clock;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView textSL, textDesconto, textSB;
     private Button buttonCalc;
     private Spinner plano;
     private EditText editSB, editDep;
     private RadioGroup va, vt, vr;
     private RadioButton radioButton;
-
+    private TextView sb_result, sl_result, inss_result, va_result, vr_result, vt_result, plano_result, irrf_result, desconto_result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +37,14 @@ public class MainActivity extends AppCompatActivity {
         vt = findViewById(R.id.vt);
         buttonCalc = findViewById(R.id.calc);
         vr = findViewById(R.id.vr);
-        textSB = findViewById(R.id.sal_brutoResult);
-
+        sb_result = findViewById(R.id.sb_result);
+        inss_result = findViewById(R.id.inss_result);
+        sl_result = findViewById(R.id.sl_result);
+        va_result = findViewById(R.id.va_result);
+        vt_result = findViewById(R.id.vt_result);
+        vr_result = findViewById(R.id.vr_result);
+        irrf_result = findViewById(R.id.irrf_result);
+        desconto_result = findViewById(R.id.desconto_result);
 
         plano.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -55,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 int radioId = va.getCheckedRadioButtonId();
                 radioButton = findViewById(radioId);
-                textSB.setText("Teste:" + radioButton.getText());
+
             }
         });
 
@@ -71,11 +80,10 @@ public class MainActivity extends AppCompatActivity {
             }else if(vt.getCheckedRadioButtonId() == -1) {
                 Toast.makeText(this, "Não Selecionou VT", Toast.LENGTH_SHORT).show();
         }else {
-                double sb, dp, sl, inss, irrf;
-                int ps = 0;
+                double sb, dp, sl, inss, irrf, ps = 0, vaValor = 0, vrValor = 0, vtValor= 0;
                 sb = Double.parseDouble(editSB.getText().toString());
                 dp = Integer.parseInt(editDep.getText().toString());
-                int planosec = plano.getSelectedItemPosition();
+
                 //Calculo do INSS
                 if (sb <= 1212.00) {
                     inss = 0.075 * sb - 0.00;
@@ -88,9 +96,8 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     inss = 713.80 - sb;
                 }
-
                 //Calculo do Plano de Saude
-                switch (planosec) {
+                switch (plano.getSelectedItemPosition()) {
                     case 0:
                         if (sb <= 3000) {
                             ps = 60;
@@ -123,8 +130,74 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
 
-                //Calculo
-                textSB.setText(String.valueOf(ps));
+                // Calculo do desconto de vale Transporte
+
+                switch (vt.getCheckedRadioButtonId()) {
+                    case 1:
+                        vtValor = sb * 0.06;
+                        break;
+                    case 2:
+                        break;
+                }
+                // Calculo do desconto de vale Alimentação.
+                switch (va.getCheckedRadioButtonId()) {
+                    case 1:
+                        if (sb <= 3000) {
+                            vaValor = 15;
+                        } else if (sb >= 3001 && sb <= 5000) {
+                            sb = 25;
+                        } else {
+                            sb = 35;
+                        }
+                        break;
+                    case 2:
+                        break;
+                }
+                // Calculo do desconto de vale Refeição.
+                switch (vr.getCheckedRadioButtonId()) {
+                    case 1:
+                        if (sb <= 3000) {
+                            vrValor = 2.60 * 22;
+                        } else if (sb >= 3001 && sb <= 5000) {
+                            vrValor = 3.65 * 22;
+                        } else {
+                            vrValor = 6.50 * 22;
+                        }
+                        break;
+                    case 2:
+                        break;
+                }
+                // Calcula o IRRF base.
+                double irrfBase = sb - inss - (189.59 * dp);
+                // Calcula o IRRF.
+                if (irrfBase <= 1903.98) {
+                    irrf = 0;
+                } else if (irrfBase <= 2826.65) {
+                    irrf = 0.075 * irrfBase - 142.80;
+                } else if (irrfBase <= 3751.05) {
+                    irrf = 0.015 * irrfBase - 354.80;
+                } else if (irrfBase <= 4664.68) {
+                    irrf = 0.225 * irrfBase - 636.13;
+                } else {
+                    irrf = 0.275 * irrfBase - 869.36;
+                }
+                //Calculo do Salário Liquido
+                sl = sb - inss - vtValor - vrValor - vaValor - irrf - ps;
+
+                //calculo dos descontos
+                double descontos = (sl - sb) * 0;
+
+                //exibição
+                sb_result.setText(String.format("Salario Bruto: %2$f", getString(R.string.sb_result), sb));
+                inss_result.setText(String.format("Inss: %2$f", getString(R.string.inss_result), inss));
+
+                va_result.setText(String.format("Vale Alimentação: %2$f", getString(R.string.va_result), vaValor));
+                vt_result.setText(String.format("Vale Transporte: %2$f", getString(R.string.vt_result), vtValor));
+                vr_result.setText(String.format("Vale Refeição: %2$f", getString(R.string.vr_result), vrValor));
+                irrf_result.setText(String.format("IRRD: %2$f", getString(R.string.irrf_result), irrf));
+                sl_result.setText(String.format("Salario Liquido: %2$f", getString(R.string.sl_result), sl));
+                desconto_result.setText(String.format("Descontos: %2$f", getString(R.string.desconto), descontos));
+
             }
             });
         }
